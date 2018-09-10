37989c42fcd5fc63903eab256961f3a95e5acbec
voinea-alex <42955789+voinea-alex@users.noreply.github.com>
2018-09-10 14:11:48 +1000

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,7 +2,7 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node {'web.puppet.vm':}
   dockeragent::node {'db.puppet.vm':}
-  dockeragent::node {'minetest.puppet.vm':}
+  dockeragent::node {'mindtest.puppet.vm':}
   host {'web.puppet.vm':
     ensure => present,
     ip     => '172.18.0.2',
